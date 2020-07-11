946e0d5a6998eee44f2519a5aaa944b9c0de81ca
patrikduch <duchpatrik@icloud.com>
2020-07-11 11:32:00 +0200

    Initial commit + Simple iterative method

---
+++ Numerical methods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Numerical_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var root =  SimpleIteration.FindRoot(0.0);
+
+
+           Console.WriteLine(root.root.ToString());
+            Console.WriteLine(root.iterations);
+
+
+           var secondRoot = SimpleIteration.FindRoot(2.0);
+
+
+           Console.WriteLine(secondRoot.root.ToString());
+            Console.WriteLine(secondRoot.iterations);
+
+
+        }
+    }
+}
---
+++ Numerical methods/starting methods/SimpleIteration.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Numerical_methods
+{
+    public class SimpleIteration
+    {
+
+        public struct SimpleIterationResult
+        {
+            public double iterations;
+            public double root;
+        }
+
+        public static SimpleIterationResult FindRoot(double initialGuess)
+        {
+            var firstIteration = initialGuess;
+            var iteration = 0;
+
+            for(iteration = 0; iteration<= 100; iteration++) 
+            {
+                double xNew;
+                if (Math.Abs(initialGuess) <= 0.0)
+                {
+                    xNew = (2*Math.Pow(firstIteration, 2) + 3) / 5;
+                } else
+                {
+                    xNew = Math.Sqrt((5 * firstIteration - 3) / 2);
+                }
+
+                if (Math.Abs(xNew - firstIteration) < 0.000001)
+                {
+                    break;
+                }
+         
+                firstIteration = xNew;       
+            }
+
+            var result = new SimpleIterationResult
+            {
+                iterations = iteration+1,
+                root = firstIteration
+            };
+
+            return result;
+
+            
+        }
+    }
+}
